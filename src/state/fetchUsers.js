8c9394d3eefa5f181d8e55e23d9425a826414555
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-15 12:05:13 +0200

    fetchUsers duck added

--- src/state/fetchUsers.js
+++ src/state/fetchUsers.js
@@ -1,8 +1,23 @@
+const SET_USERS = 'fetchUsers/SET_USERS'
+
+export const setUsersAction = data => ({
+    type: SET_USERS,
+    data 
+})
 
 const initialState = {
-
+    users: null
 }
 
+
 export default (state = initialState, action) => {
-    return state
+    switch(action.type){
+        case SET_USERS: 
+            return {
+                ...state,
+                users: action.data
+            }  
+        default: 
+            return state
+    }
 }
